3d675c020774065e4b646db72d7875933a4c34f8
WangPinChieh <pp8101313@hotmail.com>
2021-07-01 09:20:16 +0800

    format code

--- Ch1/statement.js
+++ Ch1/statement.js
@@ -1,10 +1,12 @@
-function statement (invoice, plays) {
+function statement(invoice, plays) {
     let totalAmount = 0;
     let volumeCredits = 0;
     let result = `Statement for ${invoice.customer}\n`;
     const format = new Intl.NumberFormat("en-US",
-                          { style: "currency", currency: "USD",
-                            minimumFractionDigits: 2 }).format;
+        {
+            style: "currency", currency: "USD",
+            minimumFractionDigits: 2
+        }).format;
 
     function amountFor(play, aPerformance) {
         let result = 0;
@@ -30,21 +32,21 @@ function statement (invoice, plays) {
     }
 
     for (let perf of invoice.performances) {
-      const play = plays[perf.playID];
+        const play = plays[perf.playID];
         let thisAmount = amountFor(play, perf);
 
         // add volume credits
-      volumeCredits += Math.max(perf.audience - 30, 0);
-      // add extra credit for every ten comedy attendees
-      if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
-  
-      // print line for this order
-      result += `  ${play.name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
-      totalAmount += thisAmount;
+        volumeCredits += Math.max(perf.audience - 30, 0);
+        // add extra credit for every ten comedy attendees
+        if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+
+        // print line for this order
+        result += `  ${play.name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
+        totalAmount += thisAmount;
     }
-    result += `Amount owed is ${format(totalAmount/100)}\n`;
+    result += `Amount owed is ${format(totalAmount / 100)}\n`;
     result += `You earned ${volumeCredits} credits\n`;
     return result;
-  }
+}
 
-  module.exports = statement;
+module.exports = statement;
