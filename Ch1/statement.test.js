5da08e4147c1ca2174822025244e2a5c776df88d
WangPinChieh <pp8101313@hotmail.com>
2021-07-01 09:11:28 +0800

    refactor statement tests

--- Ch1/statement.test.js
+++ Ch1/statement.test.js
@@ -1,29 +1,43 @@
 const statement = require('./statement');
-
-test('Statement should be', () => {
-    let plays = {
-        "hamlet": { "name": "Hamlet", "type": "tragedy" },
-        "as-like": { "name": "As You Like It", "type": "comedy" },
-        "othello": { "name": "Othello", "type": "tragedy" }
-    };
-    let invoice =
-    {
-        "customer": "BigCo",
-        "performances": [
-            {
-                "playID": "hamlet",
-                "audience": 55
-            },
-            {
-                "playID": "as-like",
-                "audience": 35
-            },
-            {
-                "playID": "othello",
-                "audience": 40
-            }
-        ]
+describe('Statement', function () {
+    function statementShouldBe(expected) {
+        expect(statement(this.invoice, this.plays)).toBe(expected);
     }
 
-    expect(statement(invoice, plays)).toBe("Statement for BigCo\n  Hamlet: $650.00 (55 seats)\n  As You Like It: $580.00 (35 seats)\n  Othello: $500.00 (40 seats)\nAmount owed is $1,730.00\nYou earned 47 credits\n"); 
+    let plays = {};
+
+    let invoice = {};
+
+    function givenPlays(plays) {
+        this.plays = plays;
+    }
+    function givenInvoice(invoice) {
+        this.invoice = invoice
+    }
+
+    it('should be', function () {
+        givenPlays({
+            "hamlet": {"name": "Hamlet", "type": "tragedy"},
+            "as-like": {"name": "As You Like It", "type": "comedy"},
+            "othello": {"name": "Othello", "type": "tragedy"}
+        });
+        givenInvoice({
+            "customer": "BigCo",
+            "performances": [
+                {
+                    "playID": "hamlet",
+                    "audience": 55
+                },
+                {
+                    "playID": "as-like",
+                    "audience": 35
+                },
+                {
+                    "playID": "othello",
+                    "audience": 40
+                }
+            ]
+        });
+        statementShouldBe("Statement for BigCo\n  Hamlet: $650.00 (55 seats)\n  As You Like It: $580.00 (35 seats)\n  Othello: $500.00 (40 seats)\nAmount owed is $1,730.00\nYou earned 47 credits\n");
+    });
 });
