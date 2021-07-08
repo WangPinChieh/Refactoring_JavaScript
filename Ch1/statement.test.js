1b875c5e55e73861d096793760ca9558cee7046e
WangPinChieh <pp8101313@hotmail.com>
2021-07-08 09:20:11 +0800

    add feature htmlStatement and add a test for htmlStatement

--- Ch1/statement.test.js
+++ Ch1/statement.test.js
@@ -1,21 +1,28 @@
 const statement = require('./statement');
 describe('Statement', function () {
+
+
+    let _plays = {};
+    let _invoice = {};
+    beforeEach(() => {
+        _plays = {};
+        _invoice = {};
+    });
+
     function statementShouldBe(expected) {
-        expect(statement(this.invoice, this.plays)).toBe(expected);
+        expect(statement.statement(_invoice, _plays)).toBe(expected);
     }
 
-    let plays = {};
-
-    let invoice = {};
 
     function givenPlays(plays) {
-        this.plays = plays;
-    }
-    function givenInvoice(invoice) {
-        this.invoice = invoice
+        _plays = plays;
     }
 
-    it('should be', function () {
+    function givenInvoice(invoice) {
+        _invoice = invoice
+    }
+
+    it('statement should be', function () {
         givenPlays({
             "hamlet": {"name": "Hamlet", "type": "tragedy"},
             "as-like": {"name": "As You Like It", "type": "comedy"},
@@ -40,4 +47,32 @@ describe('Statement', function () {
         });
         statementShouldBe("Statement for BigCo\n  Hamlet: $650.00 (55 seats)\n  As You Like It: $580.00 (35 seats)\n  Othello: $500.00 (40 seats)\nAmount owed is $1,730.00\nYou earned 47 credits\n");
     });
+    it('htmlStatement should be', function () {
+
+        givenPlays({
+            "hamlet": {"name": "Hamlet", "type": "tragedy"},
+            "as-like": {"name": "As You Like It", "type": "comedy"},
+            "othello": {"name": "Othello", "type": "tragedy"}
+        });
+        givenInvoice({
+            "customer": "BigCo",
+            "performances": [
+                {
+                    "playID": "hamlet",
+                    "audience": 55
+                },
+                {
+                    "playID": "as-like",
+                    "audience": 35
+                },
+                {
+                    "playID": "othello",
+                    "audience": 40
+                }
+            ]
+        });
+        expect(statement.htmlStatement(_invoice, _plays)).toBe('<h1>Statement for BigCo</h1>\n<table>\n<tr><th>play</th><th>seats</th><th>cost</th></tr> <tr><td>Hamlet</td><td>55</td><td>$650.00</td></tr>\n <tr><td>As You Like It</td><td>35</td><td>$580.00</td></tr>\n <tr><td>Othello</td><td>40</td><td>$500.00</td></tr>\n</table>\n<p>Amount owed is <em>$1,730.00</em></p>\n<p>You earned <em>47</em> credits</p>\n');
+    });
+
+
 });
